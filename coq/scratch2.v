beceb9b583afda3c504c3885e6b2682c731c03a8
Dustin Jamner <dijamner@me.com>
2019-09-22 15:08:54 -0400

    Use SSreflect style. PRove subst preserves ws.

--- coq/scratch2.v
+++ coq/scratch2.v
@@ -190,105 +190,140 @@ Qed.
 (*TODO*)
 (* Well-scoped languages
    a presupposition of well-formed languages
+   Written as functions that decide the properties
  *)
 
-Inductive ws_exp {p : polynomial} (c : ctxt p) : poly_fix p -> Prop :=
-| ws_var : forall n, n < size c -> ws_exp c (var n)
-| ws_con : forall n s v, Vector.Forall (ws_exp c) v -> ws_exp c (con n s v).
+Fixpoint ws_exp {p : polynomial} csz (e : poly_fix p) : bool :=
+  match e with
+  | var n => n < csz
+  | con n s v => Vector.fold_right (fun e f => (ws_exp csz e) && f) v true
+  end.
 
-Definition ws_ctxt_var {p : polynomial} (c : ctxt p) (v : ctxt_var p) : Prop :=
+Definition ws_ctxt_var {p : polynomial} csz (v : ctxt_var p) : bool :=
   match v with
-  | sort_var => True
-  | term_var ty => ws_exp c ty
+  | sort_var => true
+  | term_var ty => ws_exp csz ty
   end.
-Fixpoint ws_ctxt {p : polynomial} (c : ctxt p) : Prop :=
+
+Fixpoint ws_ctxt {p : polynomial} (c : ctxt p) : bool :=
   match c with
-  | [::] => True
-  | v :: c' => ws_ctxt c' /\ ws_ctxt_var c' v
+  | [::] => true
+  | v :: c' => ws_ctxt c' && ws_ctxt_var (size c) v
   end.
-Fixpoint ws_lang {p : polynomial} (l : lang p) : Prop :=
+
+Fixpoint ws_lang {p : polynomial} (l : lang p) : bool :=
   match l with
-  |[::] => True
-  | sort (c, s) :: l' => ws_lang l' /\ ws_ctxt c /\ ws_exp c s
-  | term (c, t, s) :: l' => ws_lang l' /\ ws_ctxt c /\ ws_exp c t /\ ws_exp c s
-  | sort_eq (c, (s1,s2)) :: l' => ws_lang l' /\ ws_exp c s1 /\ ws_exp c s2
-  | term_eq (c, (t1,t2), s) :: l' => ws_lang l' /\ ws_exp c t1 /\ ws_exp c t2 /\ ws_exp c s
+  |[::] => true
+  | sort (c, s) :: l' => ws_lang l' && ws_ctxt c && ws_exp (size c) s
+  | term (c, t, s) :: l' => ws_lang l' && ws_ctxt c && ws_exp (size c) t && ws_exp (size c) s
+  | sort_eq (c, (s1,s2)) :: l' => ws_lang l' && ws_exp (size c) s1 && ws_exp (size c) s2
+  | term_eq (c, (t1,t2), s) :: l' =>
+    ws_lang l' && ws_exp (size c) t1 && ws_exp (size c) t2 && ws_exp (size c) s
   end.
 
+(* replaces m variables with terms with n free vars*)
+Definition ws_subst {p} n (s : psubst p) m : bool :=
+  (List.fold_right (fun e b => ws_exp n e && b) true s) && (size s == m).
 
-Definition ws_subst {p} (c : ctxt p) (s : psubst p) : Prop :=
-  List.Forall (ws_exp c) s.
 
-Lemma ws_nth : forall p (c : ctxt p) s e n, ws_subst c s -> ws_exp c e -> ws_exp c (nth e s n).
+Lemma unandb : forall (a b : bool) (C : Prop), (a -> b -> C) <-> (a && b -> C).
 Proof.
-  move => p c; elim.
-  - move => e; elim; [done|].
-    move => n IH //=.
-  - move => x l IH e.
-    elim => [| n _].
-    + case; done.
-    + move => wss wse; apply: IH;[inversion wss|]; done.
+  move => a b C; split.
+  - move => CF ab.
+    apply andb_prop in ab.
+    case: ab.
+    done.
+  - move => CF aa bb.
+    apply CF.
+    rewrite andb_true_intro //=.
 Qed.
 
-(*TODO: subst doesn't readjust ctxt vars. This approach seems awkward*)
-(* Use Fin n for scoping?*)
-Theorem subst_preserves_ws
-  : forall {p} e s (c : ctxt p),
-    ws_exp c e ->
-    ws_subst (drop (size s) c) s ->
-    ws_exp (drop (size s) c) (exp_subst e s).
+Lemma ws_nth : forall p n (s : psubst p) m e n',
+    ws_subst n s m -> ws_exp n e -> ws_exp n (nth e s n').
 Proof.
-  move => p e s c.
-  elim => [n nlt | n s' v vfa] wse.
-  - rewrite /exp_subst lookup_nth.
-    apply: ws_nth; [done|].
-    (*TODO: need |s| < |c|*)
-    apply ws_var.
-    rewrite size_drop ltn_sub2r //=.
-    
-    Check ltn_sub2r.
-    rewrite size_drop.
-    
-    ws_subst c s -> ws_exp c e -> ws_exp c (nth e s n)
-    have nmllt : n - size s < size (drop (size s) c).
-    rewrite size_drop ltn_sub2r //=.
-    
-    rewrite <- (cat_take_drop (length s) c) in H.
-    rewrite List.app_length in H.
-    Search _ ( _ < _ + _).
-    Search length.
-    Search _ (length (_ ++ _)).
-    Search _ ((take _ _) ++ (drop _ _)).
-    remember (n < length s) as nlts.
-    destruct nlts.
-    induction s.
-   + rewrite drop0.
-     simpl.
-     auto.
-   + destruct c.
-     simpl.
-     
-     
-  
-Theorem subst_preserves_ws
-  : forall {p} e s (c1 c2 : ctxt p),
-    length s = length c1 ->
-    ws_exp c1 e ->
-    ws_subst c2 s ->
-    ws_exp c2 (exp_subst e s).
-Proof.
-  intros.
-  
-  move =>.
-  elim.
-  elim.
-  elim.  
-  simpl.
-  move => c1 c2 lz; destruct c1;[| by inversion lz].
-  move => wsz; by inversion wsz.
-  move => e s IH.
-  
+  move => p n ; elim.
+  - move => m e; elim; [done|].
+    move => n' IH //=.
+  - move => x l IH m e.
+    elim => [| n' _].
+    + rewrite /ws_subst //=.
+      repeat case /andb_prop.
+      done.
+    + rewrite /ws_subst //=.
+      repeat case /andb_prop.
+      move => _ lf wse.
+      case: m wse => //= m.
+      rewrite eqSS => wse.
+      apply: IH.
+      rewrite /ws_subst //=.
+      apply andb_true_intro; split. 
+      done.
+      exact wse.
+Qed.
 
+Lemma ws_empty : forall p n m, @ws_subst p n [::] m = (m == 0).
+Proof.
+  move => p n m.
+  rewrite /ws_subst //=.
+Qed.
+
+Lemma ws_lookup : forall p n (s : psubst p) (m n' : nat),
+    ws_subst n s m -> n' < m -> ws_exp n (var_lookup s n').
+Proof.  
+  move => p n.
+  elim.  
+  - move => m;
+    elim;
+    move: ws_empty ->;
+    [ move => /eqP -> //=
+    | move => n' IH //= /eqP => -> //=].
+  - move => e l IH m n'.
+    rewrite /ws_subst => //=.
+    (repeat case /andP) => wse sl sr.
+    elim: n' => [ //= | n' _ n'lt].
+    case: m sr n'lt => //= m.
+    move: eqSS -> => n'lt ngm.
+    apply: (IH m n') => //=.
+    rewrite /ws_subst //=.
+    move: n'lt ->.
+    rewrite Bool.andb_true_r //=.
+Qed.
+
+Ltac ws_simpl :=
+  match goal with
+    (* break up ws_subst *)
+    [ |- is_true (ws_subst _ _ _) -> _] =>
+    rewrite /ws_subst;
+    case /andP
+  end.
+
+
+Theorem subst_preserves_ws
+  : forall {p} e (s : psubst p) (c c' : ctxt p),
+    ws_exp (size c') e ->
+    ws_subst (size c) s (size c') ->
+    ws_exp (size c) (exp_subst e s).
+Proof.
+  move => p e s c c' wse.
+  move => wss.
+  pose (wss' := wss).
+  move: wss'.
+  ws_simpl.
+  move => elem_ws size_eq.
+  pose size_eq' := size_eq.
+  move: size_eq' wse => /eqP <-.
+  elim: e => //= => n.
+  - apply: ws_lookup => //=;
+    move: size_eq => /eqP -> //=.
+  - move => _.
+    elim => //= x n' v IHA IHB.
+    case /andP => xexp rexp.
+    case: IHB => IHB1 IHB2.
+    apply /andP. split; [auto|].
+    move: IHA -> => //=.
+Qed.
+
+(* TODO: from here*)
 Definition wf_ctxt {p : polynomial} (c : ctxt p) : Type := True.
 
 Fixpoint wf_lang {p : polynomial} (l : lang p) : Prop :=
