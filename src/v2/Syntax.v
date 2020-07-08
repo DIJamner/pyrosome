5ad69b6f1bed59e4e8dccb0cb17ab31f085d3286
Dustin Jamner <jamner.d@husky.neu.edu>
2020-07-08 19:26:33 -0400

    Implement substitutions and prove basic properties

--- src/v2/Syntax.v
+++ src/v2/Syntax.v
@@ -53,72 +53,91 @@ Notation subst_le := (seq term_le).
 Notation subst := (seq term).
 Notation ctx := (seq sort).
 
-Section PropIndLe.
-  (* mutual propositions to prove *)
-  Parameter Ptle : term_le -> Prop.
-  Parameter Psle : sort_le -> Prop.
 
+Fixpoint term_le_ind
+         (* mutual propositions to prove *)
+         (Ptle : term_le -> Prop)
+         (Psle : sort_le -> Prop)
+
+         (* assumptions *)
+         (IH_tle_by : forall n ps, List.Forall Ptle ps -> Ptle (tle_by n ps))
+         (IH_tle_refl_con : forall n ps, List.Forall Ptle ps -> Ptle (tle_refl_con n ps))
+         (IH_tle_refl_var : forall n, Ptle (tle_refl_var n))
+         (IH_tle_trans : forall p1 p2, Ptle p1 -> Ptle p2 -> Ptle (tle_trans p1 p2))
+         (IH_tle_conv : forall sp p, Psle sp -> Ptle p -> Ptle (tle_conv sp p))
+         
+         (IH_sle_by : forall n ps, List.Forall Ptle ps -> Psle (sle_by n ps))
+         (IH_sle_refl : forall n ps, List.Forall Ptle ps -> Psle (sle_refl n ps))
+         (IH_sle_trans : forall p1 p2, Psle p1 -> Psle p2 -> Psle (sle_trans p1 p2))
+
+         p : Ptle p :=
+  let tl_ind := term_le_ind IH_tle_by IH_tle_refl_con IH_tle_refl_var IH_tle_trans IH_tle_conv
+                            IH_sle_by IH_sle_refl IH_sle_trans in
+  let sl_ind := sort_le_ind IH_tle_by IH_tle_refl_con IH_tle_refl_var IH_tle_trans IH_tle_conv
+                            IH_sle_by IH_sle_refl IH_sle_trans in
+  let fix subst_le_ind s : List.Forall Ptle s :=
+      match s with
+      | [::] => List.Forall_nil _
+      | e::s' => List.Forall_cons _ (tl_ind e) (subst_le_ind s')
+      end in
+  match p with
+  | tle_by n ps => IH_tle_by n ps (subst_le_ind ps)
+  | tle_refl_con n ps => IH_tle_refl_con n ps (subst_le_ind ps)
+  | tle_refl_var n => IH_tle_refl_var n
+  | tle_trans p1 p2 => IH_tle_trans p1 p2 (tl_ind p1) (tl_ind p2)
+  | tle_conv sp p => IH_tle_conv sp p (sl_ind sp) (tl_ind p)
+  end
+with sort_le_ind
+       (* mutual propositions to prove *)
+       (Ptle : term_le -> Prop)
+       (Psle : sort_le -> Prop)
+
+       (* assumptions *)
+       (IH_tle_by : forall n ps, List.Forall Ptle ps -> Ptle (tle_by n ps))
+       (IH_tle_refl_con : forall n ps, List.Forall Ptle ps -> Ptle (tle_refl_con n ps))
+       (IH_tle_refl_var : forall n, Ptle (tle_refl_var n))
+       (IH_tle_trans : forall p1 p2, Ptle p1 -> Ptle p2 -> Ptle (tle_trans p1 p2))
+       (IH_tle_conv : forall sp p, Psle sp -> Ptle p -> Ptle (tle_conv sp p))
+       
+       (IH_sle_by : forall n ps, List.Forall Ptle ps -> Psle (sle_by n ps))
+       (IH_sle_refl : forall n ps, List.Forall Ptle ps -> Psle (sle_refl n ps))
+       (IH_sle_trans : forall p1 p2, Psle p1 -> Psle p2 -> Psle (sle_trans p1 p2))
+
+       sp : Psle sp :=
+       let tl_ind := term_le_ind IH_tle_by IH_tle_refl_con IH_tle_refl_var IH_tle_trans IH_tle_conv
+                                 IH_sle_by IH_sle_refl IH_sle_trans in
+       let sl_ind := sort_le_ind IH_tle_by IH_tle_refl_con IH_tle_refl_var IH_tle_trans IH_tle_conv
+                                 IH_sle_by IH_sle_refl IH_sle_trans in
+       let fix subst_le_ind s : List.Forall Ptle s :=
+           match s with
+           | [::] => List.Forall_nil _
+           | e::s' => List.Forall_cons _ (tl_ind e) (subst_le_ind s')
+           end in
+       match sp with
+       | sle_by n ps => IH_sle_by n ps (subst_le_ind ps)
+       | sle_refl n ps => IH_sle_refl n ps (subst_le_ind ps)
+       | sle_trans p1 p2 => IH_sle_trans p1 p2 (sl_ind p1) (sl_ind p2)
+       end.
+
+Combined Scheme le_ind from term_le_ind, sort_le_ind.
+
+Fixpoint term_ind (P : term -> Prop)
   (* assumptions *)
-  Parameter IH_tle_by : forall n ps, List.Forall Ptle ps -> Ptle (tle_by n ps).
-  Parameter IH_tle_refl_con : forall n ps, List.Forall Ptle ps -> Ptle (tle_refl_con n ps).
-  Parameter IH_tle_refl_var : forall n, Ptle (tle_refl_var n).
-  Parameter IH_tle_trans : forall p1 p2, Ptle p1 -> Ptle p2 -> Ptle (tle_trans p1 p2).
-  Parameter IH_tle_conv : forall sp p, Psle sp -> Ptle p -> Ptle (tle_conv sp p).
-  
-  Parameter IH_sle_by : forall n ps, List.Forall Ptle ps -> Psle (sle_by n ps).
-  Parameter IH_sle_refl : forall n ps, List.Forall Ptle ps -> Psle (sle_refl n ps).
-  Parameter IH_sle_trans : forall p1 p2, Psle p1 -> Psle p2 -> Psle (sle_trans p1 p2).
-
-  Fixpoint term_le_ind p : Ptle p :=
-    let fix subst_le_ind s : List.Forall Ptle s :=
-    match s with
-    | [::] => List.Forall_nil _
-    | e::s' => List.Forall_cons _ (term_le_ind e) (subst_le_ind s')
-    end in
-    match p with
-    | tle_by n ps => IH_tle_by n (subst_le_ind ps)
-    | tle_refl_con n ps => IH_tle_refl_con n (subst_le_ind ps)
-    | tle_refl_var n => IH_tle_refl_var n
-    | tle_trans p1 p2 => IH_tle_trans (term_le_ind p1) (term_le_ind p2)
-    | tle_conv sp p => IH_tle_conv (sort_le_ind sp) (term_le_ind p)
-    end
-  with sort_le_ind sp : Psle sp :=
-    let fix subst_le_ind s : List.Forall Ptle s :=
-    match s with
-    | [::] => List.Forall_nil _
-    | e::s' => List.Forall_cons _ (term_le_ind e) (subst_le_ind s')
-    end in
-    match sp with
-    | sle_by n ps => IH_sle_by n (subst_le_ind ps)
-    | sle_refl n ps => IH_sle_refl n (subst_le_ind ps)
-    | sle_trans p1 p2 => IH_sle_trans (sort_le_ind p1) (sort_le_ind p2)
-    end.
-
-End PropIndLe.
-
-
-Section TermPropInd.
-  (* mutual propositions to prove *)
-  Parameter P : term -> Prop.
-
-  (* assumptions *)
-  Parameter IH_var : forall x, P (var x).
-  Parameter IH_con : forall n s, List.Forall P s -> P (con n s).
-  Parameter IH_conv : forall sp e, P e -> P (conv sp e).
-
-  Fixpoint term_ind e : P e :=
+  (IH_var : forall x, P (var x))
+  (IH_con : forall n s, List.Forall P s -> P (con n s))
+  (IH_conv : forall sp e, P e -> P (conv sp e))
+   e : P e :=
     let fix subst_ind s : List.Forall P s :=
     match s with
     | [::] => List.Forall_nil _
-    | e::s' => List.Forall_cons _ (term_ind e) (subst_ind s')
+    | e::s' => List.Forall_cons _ (term_ind IH_var IH_con IH_conv e) (subst_ind s')
     end in
     match e with
     | var x => IH_var x
-    | con n s => IH_con n (subst_ind s)
-    | conv sp e => IH_conv sp (term_ind e)
+    | con n s => IH_con n s (subst_ind s)
+    | conv sp e => IH_conv sp e (term_ind IH_var IH_con IH_conv e)
     end.
 
-End TermPropInd.
 
 (* TODO: Prove*)
 Axiom term_le_eq_dec : forall p1 p2, reflect (p1 = p2) (term_le_beq p1 p2).
@@ -144,7 +163,7 @@ Definition sort_to_refl (t : sort) : sort_le :=
   match t with srt n s => sle_refl n (subst_to_refl s) end.
 
 Definition var_lookup (s : subst) (n : nat) : term :=
-  nth_level (var n) s n.
+  nth_level (con 0 [::]) s n.
 Global Transparent var_lookup.
 
 Definition var_lookup_type (c : ctx) (n : nat) : sort :=
@@ -152,7 +171,7 @@ Definition var_lookup_type (c : ctx) (n : nat) : sort :=
 Global Transparent var_lookup_type.
 
 Definition var_lookup_le (s : subst_le) (n : nat) : term_le :=
-  nth_level (tle_refl_var n) s n.
+  nth_level (tle_refl_con 0 [::]) s n.
 Global Transparent var_lookup_le.
 
 (* TODO: the right way (?): subst subst_Les into term_les
@@ -189,163 +208,184 @@ Definition sort_subst s (t : sort) : sort :=
   let (n, s') := t in
   srt n (subst_subst s s').
 
+Lemma subst_le_cmp_size s1 s2 : size (subst_le_subst s2 s1) = size s1.
+Proof using .
+  elim: s1; simpl; eauto.
+Qed.
+
+Lemma subst_cmp_size s1 s2 : size (subst_subst s2 s1) = size s1.
+Proof using .
+  elim: s1; simpl; eauto.
+Qed.
+
+Lemma var_lookup_le_cmp n s1 s2  : term_le_subst s2 (var_lookup_le s1 n) = var_lookup_le (subst_le_subst s2 s1) n.
+Proof using .
+  unfold var_lookup_le.
+  unfold nth_level.
+  rewrite subst_le_cmp_size.
+  case (n < size s1); simpl; eauto.
+  generalize (size s1 - n.+1) => n'.
+  elim: n' s1; intros until s1; case: s1; simpl; eauto.
+Qed.
+
+Lemma var_lookup_cmp n s1 s2  : term_subst s2 (var_lookup s1 n) = var_lookup (subst_subst s2 s1) n.
+Proof using .
+  unfold var_lookup.
+  unfold nth_level.
+  rewrite subst_cmp_size.
+  case (n < size s1); simpl; eauto.
+  generalize (size s1 - n.+1) => n'.
+  elim: n' s1; intros until s1; case: s1; simpl; eauto.
+Qed.
+  
+Lemma le_successive_subst_cmp
+  : (forall (e : term_le) s1 s2, term_le_subst s2 (term_le_subst s1 e) = term_le_subst (subst_le_subst s2 s1) e)
+    /\ ((forall (e : sort_le) s1 s2, sort_le_subst s2 (sort_le_subst s1 e) = sort_le_subst (subst_le_subst s2 s1) e)).
+Proof using .
+  eapply le_ind; simpl; intros; f_equal; auto;
+    try by apply: var_lookup_le_cmp.
+  all: elim: ps H; simpl; eauto; intro_to List.Forall; inversion; subst; f_equal; eauto.
+Qed.
+
+Lemma subst_to_refl_size s : size (subst_to_refl s) = size s.
+Proof using .
+  elim: s; simpl; eauto.
+Qed.
+
+Lemma lookup_to_refl s x : var_lookup_le (subst_to_refl s) x = term_to_refl (var_lookup s x).
+Proof using.
+  unfold var_lookup_le.
+  unfold var_lookup.
+  unfold nth_level.
+  rewrite subst_to_refl_size.
+  case (x < size s); simpl; eauto.
+  generalize (size s - x.+1) => x'.
+  elim: x' s; intros until s; case: s; simpl; auto.  
+Qed.
+
+Lemma term_to_refl_distributes s e : term_le_subst (subst_to_refl s) (term_to_refl e) = term_to_refl (term_subst s e).
+Proof using .
+  elim: e; simpl; auto; intros; (try by apply: lookup_to_refl); f_equal; auto.
+  elim: s0 H; simpl; auto; intro_to List.Forall; inversion; subst; f_equal; auto.
+Qed.  
+
+Lemma subst_to_refl_distributes s1 s2
+  : subst_le_subst (subst_to_refl s2) (subst_to_refl s1) = subst_to_refl (subst_subst s2 s1).
+Proof using .
+  elim: s1 s2; simpl; eauto; intros; f_equal; eauto using term_to_refl_distributes.
+Qed.
+
+Lemma le_successive_subst_cmp_refl
+  : (forall (e : term_le) s1 s2,
+        term_le_subst (subst_to_refl s2) (term_le_subst (subst_to_refl s1) e)
+        = term_le_subst (subst_to_refl (subst_subst s2 s1)) e)
+    /\ (forall (e : sort_le) s1 s2,
+        sort_le_subst (subst_to_refl s2) (sort_le_subst (subst_to_refl s1) e)
+        = sort_le_subst (subst_to_refl (subst_subst s2 s1)) e).
+Proof using .
+  split; intros; rewrite -!subst_to_refl_distributes; eapply le_successive_subst_cmp.
+Qed.
+
+Lemma subst_le_assoc s1 s2 s3
+  : subst_le_subst (subst_le_subst s3 s2) s1 = subst_le_subst s3 (subst_le_subst s2 s1).
+Proof using .
+  elim: s1; simpl; intros; f_equal; auto.
+  by rewrite (proj1 le_successive_subst_cmp).
+Qed.
+
+Lemma term_successive_subst_cmp
+  : forall (e : term) s1 s2, term_subst s2 (term_subst s1 e) = term_subst (subst_subst s2 s1) e.
+Proof using .
+  elim; simpl; intros; f_equal; auto using var_lookup_cmp.
+  - elim: s H; simpl; intro_to List.Forall; inversion; subst; f_equal; auto.
+  - by rewrite (proj2 le_successive_subst_cmp_refl).
+Qed.
+
+Lemma subst_assoc s1 s2 s3
+  : subst_subst (subst_subst s3 s2) s1 = subst_subst s3 (subst_subst s2 s1).
+Proof using .
+  elim: s1; simpl; intros; f_equal; auto.
+  by rewrite term_successive_subst_cmp.
+Qed.
+
+Lemma sort_successive_subst_cmp
+  : forall (e : sort) s1 s2, sort_subst s2 (sort_subst s1 e) = sort_subst (subst_subst s2 s1) e.
+Proof using .
+  case; simpl; intros; f_equal; auto.
+  by rewrite subst_assoc.
+Qed.
 
 Class Substable (sub : Set) (stx : Set) : Set :=
   { apply_subst : sub -> stx -> stx
-    (* TODO: properties *)
+    ; subst_cmp : sub -> sub -> sub
+    ; successive_subst_cmp : forall e s1 s2, apply_subst s2 (apply_subst s1 e) = apply_subst (subst_cmp s2 s1) e
+    ; subst_cmp_assoc : forall s1 s2 s3, subst_cmp (subst_cmp s3 s2) s1 = subst_cmp s3 (subst_cmp s2 s1)
   }.
 Notation "e [/ s /]" := (apply_subst s e) (at level 7, left associativity).
 
 Instance term_le_Substable : Substable subst_le term_le :=
-  { apply_subst := term_le_subst }.
+  { apply_subst := term_le_subst
+    ; subst_cmp := subst_le_subst
+    ; successive_subst_cmp := proj1 le_successive_subst_cmp
+    ; subst_cmp_assoc := subst_le_assoc
+  }.
+  
 Instance term_le_Substable_refl : Substable subst term_le :=
-  { apply_subst s := term_le_subst (subst_to_refl s) }.
+  { apply_subst s := term_le_subst (subst_to_refl s)
+    ; subst_cmp := subst_subst
+    ; successive_subst_cmp := proj1 le_successive_subst_cmp_refl
+    ; subst_cmp_assoc := subst_assoc }.
+
 
 Instance sort_le_Substable : Substable subst_le sort_le :=
-  { apply_subst := sort_le_subst }.
+  { apply_subst := sort_le_subst
+    ; subst_cmp := subst_le_subst
+    ; successive_subst_cmp := proj2 le_successive_subst_cmp
+    ; subst_cmp_assoc := subst_le_assoc }.
+
 Instance sort_le_Substable_refl : Substable subst sort_le :=
-  { apply_subst s := sort_le_subst (subst_to_refl s) }.
+  { apply_subst s := sort_le_subst (subst_to_refl s) 
+    ; subst_cmp := subst_subst
+    ; successive_subst_cmp := proj2 le_successive_subst_cmp_refl
+    ; subst_cmp_assoc := subst_assoc }.
 
-Instance subst_le_Substable : Substable subst_le subst_le :=
-  { apply_subst := subst_le_subst }.
-Instance subst_le_Substable_refl : Substable subst subst_le :=
-  { apply_subst s := subst_le_subst (subst_to_refl s) }.
+#[refine] Instance subst_le_Substable : Substable subst_le subst_le :=
+  { apply_subst := subst_le_subst
+    ; subst_cmp := subst_le_subst
+    ; subst_cmp_assoc := subst_le_assoc }.
+Proof.
+  intros; by rewrite subst_le_assoc.
+Defined.
 
+#[refine] Instance subst_le_Substable_refl : Substable subst subst_le :=
+  { apply_subst s := subst_le_subst (subst_to_refl s) 
+    ; subst_cmp := subst_subst 
+    ; subst_cmp_assoc := subst_assoc }.
+Proof.
+  pose tssp := proj1 le_successive_subst_cmp_refl.
+  elim; simpl; intros; f_equal; eauto.
+Defined.
+  
 Instance term_Substable : Substable subst term :=
-  { apply_subst := term_subst }.
-
+  { apply_subst := term_subst 
+    ; subst_cmp := subst_subst
+    ; successive_subst_cmp := term_successive_subst_cmp
+    ; subst_cmp_assoc := subst_assoc }.
+  
 Instance sort_Substable : Substable subst sort :=
-  { apply_subst := sort_subst }.
+  { apply_subst := sort_subst 
+    ; subst_cmp := subst_subst
+    ; successive_subst_cmp := sort_successive_subst_cmp
+    ; subst_cmp_assoc := subst_assoc }.
 
-Instance subst_Substable : Substable subst subst :=
-  { apply_subst := subst_subst }.
+#[refine] Instance subst_Substable : Substable subst subst :=
+  { apply_subst := subst_subst 
+    ; subst_cmp := subst_subst
+    ; subst_cmp_assoc := subst_assoc }.
+Proof.
+  intros; by rewrite subst_assoc.
+Defined.
 
-
-Scheme term_le_ind := Induction for term_le Sort Prop.
-Check term_le_ind.
-
-  with wf_subst_mono_subst_ind := Minimality for wf_subst Sort Prop
-  with wf_term_mono_subst_ind := Minimality for wf_term Sort Prop.
-
-Combined Scheme mono_subst_ind from
-wf_sort_mono_subst_ind,
-wf_subst_mono_subst_ind,
-wf_term_mono_subst_ind.
-
-Print List.Forall.
-Fixpoint exp_ind
-         (P : exp -> Prop)
-         (IHV : forall n, P(var n))
-         (IHC : forall n l,
-             List.Forall P l ->
-             P (con n l))
-         (IHCV : forall pf e,
-             P e -> P (conv pf e))
-         (e : exp) { struct e} : P e :=
-  match e with
-  | var n => IHV n
-  | con n l =>
-    let fix loop l :=
-        match l return List.Forall P l with
-        | [::] => List.Forall_nil _
-        | e' :: l' => List.Forall_cons _ (exp_ind IHV IHC IHCV e') (loop l')
-        end in
-    IHC n l (loop l)
-  | conv pf e =>
-    IHCV pf e (exp_ind IHV IHC IHCV e)
-  end.
-
-Fixpoint exp_rect
-         (P : exp -> Type)
-         (IHV : forall n, P(var n))
-         (IHC : forall n l,
-             List.fold_right (fun t : exp => prod (P t)) unit l ->
-             P (con n l))
-         (IHCV : forall pf e,
-             P e -> P (conv pf e))
-         (e : exp) { struct e} : P e :=
-  match e with
-  | var n => IHV n
-  | con n l =>
-    let fix loop l :=
-        match l return List.fold_right (fun t => prod (P t)) unit l with
-        | [::] => tt
-        | e' :: l' => (exp_rect IHV IHC IHCV e', loop l')
-        end in
-    IHC n l (loop l)
-  | conv pf e =>
-    IHCV pf e (exp_rect IHV IHC IHCV e)
-  end.
-
-Definition exp_rec := 
-[eta exp_rect]
-     : forall P : exp -> Set,
-       (forall n : nat, P (var n)) ->
-       (forall n l,
-             List.fold_right (fun t : exp => prod (P t)) unit l ->
-             P (con n l))->
-       (forall pf e,
-             P e -> P (conv pf e)) -> forall e : exp, P e.
-
-Notation "[ n | ]" := (con n [::]).
-Notation "[ n | e ]" := (con n [:: e]).
-Notation "[ n | e ; .. ; e' ]" := (con n (cons e .. (cons e' [::]) ..)).
-
-(* TODO: what's the right thing to do? need intuition from denotations, compilers
-Variant constr (T : Set) : Set :=
-| ccon : nat -> seq T -> constr T.
-
-Notation "[{ T } n | ]" := (ccon T n [::]).
-Notation "[{ T } n | e ]" := (ccon T n [:: e]).
-Notation "[{ T } n | e ; .. ; e' ]" := (ccon T n (cons e .. (cons e' [::]) ..)).
-
-Definition Alg T := constr T -> T.
-
-Fixpoint alg_fn (a : Alg) (e : exp) : exp :=
-  match e with
-  | var n => var n
-  | con n l => a (ccon n (map (alg_fn a) l))
-  end.    
-
-Definition id_alg : Alg := fun ce =>
-  match ce with
-  | ccon n l => con n l
-  end.
-*)
-
-(* TODO: uniform inheritance condition? this feels like it would be nice to have
-Coercion id_alg : constr >-> exp. *)
-
-Definition ctx := list exp.
-
-
-Definition subst := seq exp.
-
-Definition var_lookup (c : subst) (n : nat) : exp :=
-  nth_level (var n) c n.
-Global Transparent var_lookup.
-
-Fixpoint exp_var_map (f : nat -> exp) (e : exp) : exp :=
-  match e with
-  | var n => f n
-  | con n l => con n (map (exp_var_map f) l)
-  | conv pf e' => conv pf (exp_var_map f e')
-  end.
-
-Definition exp_subst (s : subst) e : exp :=
-  exp_var_map (var_lookup s) e.
-
-Notation "e [/ s /]" := (exp_subst s e) (at level 7, left associativity).
-
-Definition subst_cmp s1 s2 : subst := map (exp_subst s2) s1.
-
-
-Lemma con_subst_cmp n s s0 : (con n s0)[/s/] = con n (subst_cmp s0 s).
-Proof using .
-  unfold exp_subst.
-  simpl.
-  f_equal.
-Qed.
 
 
 Definition exp_shift (m : nat) e : exp :=
