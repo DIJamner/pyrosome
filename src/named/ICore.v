2e06d2a5606a46d63f42bd9e76bef18e9314e74b
Dustin Jamner <dijamner@me.com>
2021-01-13 19:49:02 -0500

    Fix bug in le_args

--- src/named/ICore.v
+++ src/named/ICore.v
@@ -75,13 +75,13 @@ Inductive le_args (l : lang) c : ctx -> list exp -> list exp -> list string -> l
     forall name t e1 e2,
       (* assumed because the output ctx is wf: fresh name c' ->*)
       le_term l c t[/with_names_from c' es2/] e1 e2 ->
-      le_args l c ((name, t)::c') (e1::s1) (e2::s2) (name::args) (e1::es1) (e1::es2)
+      le_args l c ((name, t)::c') (e1::s1) (e2::s2) (name::args) (e1::es1) (e2::es2)
 | le_args_cons_im : forall c' s1 s2 args es1 es2,
     le_args l c c' s1 s2 args es1 es2 ->
     forall name t e1 e2,
       (* assumed because the output ctx is wf: fresh name c' ->*)
       le_term l c t[/with_names_from c' es2/] e1 e2 ->
-      le_args l c ((name, t)::c') s1 s2 args (e1::es1) (e1::es2).
+      le_args l c ((name, t)::c') s1 s2 args (e1::es1) (e2::es2).
 
 Inductive wf_term l c : exp -> sort -> Prop :=
 | wf_term_by : forall n s args es c' t,
