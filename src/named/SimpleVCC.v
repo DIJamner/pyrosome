fef2642df1bbfc1840a4b992a8338acb0a096ce7
Dustin Jamner <dijamner@me.com>
2021-06-12 21:52:54 -0400

    Progress on closure conversion pass

--- src/named/SimpleVCC.v
+++ src/named/SimpleVCC.v
@@ -204,6 +204,16 @@ Definition cc_lang_def : lang :=
       = #"blk_subst" (#"pair" %"v" %"v'") %"e"
       : #"blk" %"G"
   ];
+  [:> "G" : #"ty",
+      "A" : #"ty",
+      "v" : #"val" %"G" (#"neg" %"A")
+      ----------------------------------------------- ("clo_eta")
+      (#"closure" %"A"
+        (#"jmp" (#"val_subst" #".1" %"v") #".2")
+        #"id")
+      = %"v"
+      : #"val" %"G" (#"neg" %"A")
+  ];
   [:> "G" : #"ty", "A" : #"ty",
       "v1" : #"val" %"G" (#"neg" %"A"),
       "v2" : #"val" %"G" %"A",
@@ -237,11 +247,215 @@ Proof. auto_elab. Qed.
 
 
 Definition value_cc_def : compiler :=
-  match # from (value_cc_subst) with
-    
+  match # from (value_subst) with
+  | {{s #"env" }} => {{s #"ty"}}
+  | {{s #"sub" "A" "B"}} => {{s #"val" %"A" %"B"}}
+  | {{e #"cmp" "G" "G'" "A" "g" "g'"}} =>
+    {{e #"val_subst" %"g" %"g'"}}
+  | {{e #"emp"}} => {{e#"unit"}}
+  | {{e #"forget"}} => {{e# "tt"}}
+  | {{e #"ext" "A" "B" }} => {{e #"prod" %"A" %"B"}}
+  | {{e #"snoc" "G" "G'" "A" "g" "v"}} =>
+    {{e #"pair" %"g" %"v"}}
+  | {{e #"hd" "G" "A"}} => {{e #".2"}}
+  | {{e #"wkn" "G" "A"}} => {{e #".1"}}
   end.
 
 
+Derive value_cc
+       SuchThat (elab_preserving_compiler []
+                                          (prod_cc ++ value_cc_subst)
+                                          value_cc_def
+                                          value_cc
+                                          value_subst)
+       As value_cc_preserving.
+Proof.
+  (*TODO: make specialized tactic that doesn't need depth*)
+  assert (wf_lang (prod_cc++value_cc_subst))
+    by eauto 8 with auto_elab elab_pfs.
+
+  setup_elab_compiler.
+
+  all: repeat t.
+  all: solve[by_reduction].
+  Unshelve.
+  all: repeat t'; eauto with elab_pfs auto_elab.
+  all: simpl.
+  all: repeat t'; eauto with elab_pfs auto_elab.
+  (*TODO: why isn't everything simplified already?*)
+Qed.
+(*
+  auto_elab_compiler.
+Qed.
+*)
+#[export] Hint Resolve value_cc_preserving : elab_pfs.
+
+
+
+Definition prod_cc_compile_def : compiler :=
+  match # from (val_prod_lang) with
+  | {{e #".1" "G" "A" "B" "v"}} => {{e #"val_subst" %"v" #".1"}}
+  | {{e #".2" "G" "A" "B" "v"}} => {{e #"val_subst" %"v" #".2"}}
+  end.
+
+
+(*TODO: only works if all variables appear on the rhs*)
+Ltac redex_steps_with_rhs lang name :=
+  let mr := eval compute in (named_list_lookup_err lang name) in
+      lazymatch mr with
+      | Some (term_eq_rule ?c ?e1p ?e2p ?tp) =>
+        lazymatch goal with
+        | [|- eq_term ?l ?c' ?t ?e1 ?e2] =>
+          let ms := eval compute in (matches e2 e2p (map fst c)) in
+              lazymatch ms with
+              | Some ?s =>
+                replace (eq_term l c' t e1 e2)
+                  with (eq_term l c' tp[/s/] e1p[/s/] e2p[/s/]);
+                [| reflexivity];
+                eapply eq_term_subst;
+                [| | eq_term_by name];
+                [solve [repeat t']|apply eq_subst_refl; solve [repeat t'] ]
+              | None => fail "rhs" e2 "does not match rule" e2p
+              end
+        | _ => fail "Goal not a term equality"
+        end
+      | _ => fail "Rule not found"
+      end.
+
+Derive prod_cc_compile
+       SuchThat (elab_preserving_compiler value_cc
+                                          (prod_cc ++ value_cc_subst)
+                                          prod_cc_compile_def
+                                          prod_cc_compile
+                                          val_prod_lang)
+       As prod_cc_preserving.
+Proof.
+  (*TODO: make specialized tactic that doesn't need depth*)
+  assert (wf_lang (prod_cc++value_cc_subst))
+    by eauto 8 with auto_elab elab_pfs.
+
+  setup_elab_compiler.
+
+  all: repeat t.
+  solve[by_reduction].
+  solve[by_reduction].
+  {
+    compute_eq_compilation.
+      
+    eapply eq_term_trans.
+    eapply eq_term_sym.
+    redex_steps_with_rhs prod_cc "subst pair".
+    compute_eq_compilation.
+    eapply eq_term_trans.
+    {
+      term_cong.
+      term_refl.
+      term_refl.
+      term_refl.
+      term_refl.
+      compute_eq_compilation.
+      redex_steps_with prod_cc "pair eta".
+    }
+    by_reduction.
+  }
+  all: solve[by_reduction].
+  Unshelve.
+  all: repeat t'; eauto with elab_pfs auto_elab.
+  all: simpl.
+  all: repeat t'; eauto with elab_pfs auto_elab.
+  (*TODO: why isn't everything simplified already?*)
+Qed.
+(*
+  auto_elab_compiler.
+Qed.
+*)
+#[export] Hint Resolve prod_cc_preserving : elab_pfs.
+
+
+Definition block_cc_def : compiler :=
+  match # from (block_subst) with
+  | {{s #"blk" "G"}} => {{s #"blk" %"G"}}
+  end.
+
+
+Derive block_cc
+       SuchThat (elab_preserving_compiler value_cc
+                                          (block_cc_subst ++ value_cc_subst)
+                                          block_cc_def
+                                          block_cc
+                                          block_subst)
+       As block_cc_preserving.
+Proof.
+  (*TODO: make specialized tactic that doesn't need depth*)
+  assert (wf_lang (block_cc_subst++value_cc_subst))
+    by eauto 8 with auto_elab elab_pfs.
+
+  setup_elab_compiler.
+
+  all: repeat t.
+  all:solve[by_reduction].
+  Unshelve.
+  all: repeat t'; eauto with elab_pfs auto_elab.
+  all: simpl.
+  all: repeat t'; eauto with elab_pfs auto_elab.
+  (*TODO: why isn't everything simplified already?*)
+Qed.
+#[export] Hint Resolve block_cc_preserving : elab_pfs.
+
+
+
+Definition cc_def : compiler :=
+  match # from (cps_lang) with
+  | {{e #"neg" "A" }} => {{e #"neg" %"A"}}
+  | {{e #"cont" "G" "A" "e"}} =>
+    {{e #"closure" %"A" %"e" #"id"}}
+  | {{e #"jmp" "G" "A" "v1" "v2"}} =>
+    {{e #"jmp" %"v1" %"v2" }}
+  end.
+
+
+Derive cc
+       SuchThat (elab_preserving_compiler (block_cc ++ value_cc)
+                                          (cc_lang ++  block_cc_subst ++ prod_cc ++ value_cc_subst)
+                                          cc_def
+                                          cc
+                                          cps_lang)
+       As block_cc_preserving.
+Proof.
+  (*TODO: make specialized tactic that doesn't need depth*)
+  assert (wf_lang (cc_lang++ block_cc_subst++prod_cc ++value_cc_subst))
+    by eauto 25 with auto_elab elab_pfs.
+
+  setup_elab_compiler.
+
+  all: repeat t.
+  solve[by_reduction].
+  solve[by_reduction].
+  {
+    compute_eq_compilation.
+    eapply eq_term_trans.
+    redex_steps_with cc_lang "clo_subst".
+    (*TODO: why does this error? step_if_concrete.*)
+    compute_eq_compilation.
+    eapply eq_term_trans.
+    eapply eq_term_sym.
+    admit.
+    (* TODO: doesn't work b/c not all vars are instantiated
+      redex_steps_with_rhs cc_lang "clo_eta".*)
+    admit.
+  }
+  Unshelve.
+  all: repeat t'; eauto with elab_pfs auto_elab.
+  all: simpl.
+  all: repeat t'; eauto with elab_pfs auto_elab.
+  constructor; exact "".
+  (*TODO: why isn't everything simplified already?*)
+Qed.
+#[export] Hint Resolve block_cc_preserving : elab_pfs.
+
+
+
+  
 (*TODO: put in hoisting file*)
 Definition hoist_lang_def : lang :=
   [[l
