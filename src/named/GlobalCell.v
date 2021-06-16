ad7d7ba6fd1f6c13be6bd3d62f7d49f02e751ba9
Dustin Jamner <dijamner@me.com>
2021-06-16 11:41:23 -0400

    Move Hoisting to separate file. Get all files in _CoqProject building

--- src/named/GlobalCell.v
+++ src/named/GlobalCell.v
@@ -14,86 +14,86 @@ Require Coq.derive.Derive.
 Set Default Proof Mode "Classic".
 
 (*depends on subst*)
-Definition bool_lang :=
+Definition bool_lang_def :=
   [
     (*TODO: add if; add subst rules*)
     [:| "G" : #"env"
         -----------------------------------------------
-        #"false" : #"val" %"G" #"bool"
+        #"false" : #"val" "G" #"bool"
      ];
     [:| "G" : #"env"
         -----------------------------------------------
-        #"true" : #"val" %"G" #"bool"
+        #"true" : #"val" "G" #"bool"
      ];
     [:| 
         -----------------------------------------------
         #"bool" : #"ty"
-  ]]%arule.
+  ]]%rule.
 
-Derive bool_elab
-       SuchThat (Pre.elab_lang subst_elab bool_lang bool_elab)
+Derive bool_lang
+       SuchThat (Pre.elab_lang (exp_subst ++ value_subst) bool_lang_def bool_lang)
        As bool_lang_wf.
 Proof. auto_elab. Qed.
 #[export] Hint Resolve bool_lang_wf : elab_pfs.
 
 
 (*depends on bool, eval_ctx*)
-Definition global_cell :=
+Definition global_cell_def :=
   [
-  [:> "S" : #"val" #"emp" #"bool",
+  [:= "S" : #"val" #"emp" #"bool",
       "v" : #"val" #"emp" #"bool",
       "A" : #"ty",            
-      "E" : #"Ectx" #"emp" #"bool" %"A"
+      "E" : #"Ectx" #"emp" #"bool" "A"
       ----------------------------------------------- ("eval_swap")
-      #"mk_config" %"S" (#"plug" %"E" (#"swap" %"v"))
-      = #"mk_config" %"v" (#"plug" %"E" (#"ret" %"S"))
-      : #"config" %"A"
+      #"mk_config" "S" (#"plug" "E" (#"swap" "v"))
+      = #"mk_config" "v" (#"plug" "E" (#"ret" "S"))
+      : #"config" "A"
   ];
-  [:> "S" : #"val" #"emp" #"bool",
+  [:= "S" : #"val" #"emp" #"bool",
       "A" : #"ty",            
-      "E" : #"Ectx" #"emp" #"bool" %"A"
+      "E" : #"Ectx" #"emp" #"bool" "A"
       ----------------------------------------------- ("eval_get")
-      #"mk_config" %"S" (#"plug" %"E" #"get")
-      = #"mk_config" %"S" (#"plug" %"E" (#"ret" %"S"))
-      : #"config" %"A"
+      #"mk_config" "S" (#"plug" "E" #"get")
+      = #"mk_config" "S" (#"plug" "E" (#"ret" "S"))
+      : #"config" "A"
   ];
     [:| "S" : #"val" #"emp" #"bool",
-        "A" : #"ty", "e" : #"el" #"emp" %"A"                    
+        "A" : #"ty", "e" : #"exp" #"emp" "A"                    
        -----------------------------------------------
-       #"mk_config" "S" "e" : #"config" %"A"
+       #"mk_config" "S" "e" : #"config" "A"
   ];
     [s| "A" : #"ty"                   
        -----------------------------------------------
        #"config" "A" srt
   ];
-  [:> "G" : #"env",
-      "v" : #"val" %"G" #"bool",
+  [:= "G" : #"env",
+      "v" : #"val" "G" #"bool",
       "G'" : #"env",
-      "g" : #"sub" %"G'" %"G"
+      "g" : #"sub" "G'" "G"
       ----------------------------------------------- ("swap_subst")
-      #"el_subst" %"g" (#"swap" %"v")
-      = #"swap" (#"val_subst" %"g" %"v")
-      : #"el" %"G'" #"bool"
+      #"exp_subst" "g" (#"swap" "v")
+      = #"swap" (#"val_subst" "g" "v")
+      : #"exp" "G'" #"bool"
   ];
-  [:> "G" : #"env",
+  [:= "G" : #"env",
       "G'" : #"env",
-      "g" : #"sub" %"G'" %"G"
+      "g" : #"sub" "G'" "G"
       ----------------------------------------------- ("get_subst")
-      #"el_subst" %"g" (#"get") = #"get" : #"el" %"G'" #"bool"
+      #"exp_subst" "g" (#"get") = #"get" : #"exp" "G'" #"bool"
   ];
-  [:| "G" : #"env", "v" : #"val" %"G" #"bool"
+  [:| "G" : #"env", "v" : #"val" "G" #"bool"
        -----------------------------------------------
-       #"swap" "v" : #"el" %"G" #"bool"
+       #"swap" "v" : #"exp" "G" #"bool"
   ];
      [:| "G" : #"env"
        -----------------------------------------------
-       #"get" : #"el" %"G" #"bool"
-  ]]%arule.
+       #"get" : #"exp" "G" #"bool"
+  ]]%rule.
 
-Derive global_cell_elab
-       SuchThat (Pre.elab_lang (bool_elab ++ eval_ctx_elab ++ subst_elab)
-                               global_cell
-                               global_cell_elab)
+Derive global_cell
+       SuchThat (Pre.elab_lang (bool_lang ++ eval_ctx ++ (exp_subst ++ value_subst))
+                               global_cell_def
+                               global_cell)
        As global_cell_wf.
 Proof. auto_elab. Qed.
 #[export] Hint Resolve global_cell_wf : elab_pfs.
