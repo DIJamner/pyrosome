4b9f04c4b74c782650ddbab2208d46454fbd896e
Dustin Jamner <dijamner@me.com>
2021-07-05 11:32:50 -0400

    Make fix extend STLC

--- src/named/SimpleVFix.v
+++ src/named/SimpleVFix.v
@@ -6,7 +6,7 @@ Import ListNotations.
 Open Scope string.
 Open Scope list.
 From Utils Require Import Utils.
-From Named Require Import Core Elab Matches SimpleVSubst.
+From Named Require Import Core Elab Matches SimpleVSubst SimpleVSTLC.
 From Named Require ElabWithPrefix.
 Module Pre := ElabWithPrefix.
 Import Core.Notations.
@@ -15,24 +15,12 @@ Require Coq.derive.Derive.
 
 Definition fix_def : lang :=
   {[l
-  [:| "t" : #"ty", "t'": #"ty"
-      -----------------------------------------------
-      #"->" "t" "t'" : #"ty"
-  ];
   [:| "G" : #"env",
        "A" : #"ty",
        "B" : #"ty",
        "e" : #"exp" (#"ext" (#"ext" "G" (#"->" "A" "B")) "A") "B"
        -----------------------------------------------
        #"fix" "A" "e" : #"val" "G" (#"->" "A" "B")
-  ];
-  [:| "G" : #"env",
-       "A" : #"ty",
-       "B" : #"ty",
-       "e" : #"exp" "G" (#"->" "A" "B"),
-       "e'" : #"exp" "G" "A"
-       -----------------------------------------------
-       #"app" "e" "e'" : #"exp" "G" "B"
   ];
   [:= "G" : #"env",
       "A" : #"ty",
@@ -43,16 +31,6 @@ Definition fix_def : lang :=
       #"app" (#"ret" (#"fix" "A" "e")) (#"ret" "v")
       = #"exp_subst" (#"snoc" (#"snoc" #"id" (#"fix" "A" "e")) "v") "e"
       : #"exp" "G" "B"
-  ];
-  [:= "G" : #"env", "A" : #"ty", "B" : #"ty",
-      "e" : #"exp" "G" (#"->" "A" "B"),
-      "e'" : #"exp" "G" "A",
-      "G'" : #"env",
-      "g" : #"sub" "G'" "G"
-      ----------------------------------------------- ("app_subst")
-      #"exp_subst" "g" (#"app" "e" "e'")
-      = #"app" (#"exp_subst" "g" "e") (#"exp_subst" "g" "e'")
-      : #"exp" "G'" "B"
   ];
   [:= "G" : #"env", "A" : #"ty", "B" : #"ty",
       "e" : #"exp" (#"ext" (#"ext" "G" (#"->" "A" "B")) "A") "B",
@@ -66,7 +44,7 @@ Definition fix_def : lang :=
   ]}.
 
 Derive fix_lang
-       SuchThat (Pre.elab_lang (exp_subst++value_subst) fix_def fix_lang)
+       SuchThat (Pre.elab_lang (stlc++exp_subst++value_subst) fix_def fix_lang)
        As fix_wf.
 Proof. auto_elab. Qed.
 #[export] Hint Resolve fix_wf : elab_pfs.
