50c50dff81a068f0509e71819632e72dc2b6c04f
Dustin Jamner <dijamner@me.com>
2021-09-06 22:21:57 -0400

    Implement UnionFind data structure. TODO: needs coq issue fixed. TODO: prove lemmas

--- src/Utils/UnionFind.v
+++ src/Utils/UnionFind.v
@@ -3,6 +3,11 @@
    The instantiation with persistent arrays should be performant,
    while the instantiation with a function implementation should be closed
    under the global context.
+
+   based on this paper:
+   Sylvain Conchon and Jean-Christophe Filliâtre. A persistent union-find data structure.
+   In ACM SIGPLAN Workshop on ML, 37–45. Freiburg, Germany, October 2007. ACM Press.
+   URL: https://www.lri.fr/~filliatr/ftp/publis/puf-wml07.pdf.
 *)
 Require Import Equalities Orders ZArith.
 
@@ -11,18 +16,102 @@ From Utils Require Import Natlike ArrayList.
 
 (* All definitions and proofs need to be generic over arraylist implementations.
    Use the following code to test functions with a specific implementation: *)
-Module UnionFind.   
-  From Utils Require PersistentArrayList.
-  Import PersistentArrayList.Int63Natlike.
-  Import PersistentArrayList.PArrayList.
 
-(* Use this module declaration for proofs.
 Module UnionFind
+       (* TODO: can't abstract for now due to issue #14849
+       (Import NL : Natlike)
+       (Import AL : ArrayList NL)*).
+  (*Temporary imports*)
+  From Utils Require PersistentArrayList.
+  Module NL := PersistentArrayList.Int63Natlike.
+  Module AL := PersistentArrayList.PArrayList.
+  
+  Module Ntns := (ArrayNotations NL AL).
+  Import NL AL Ntns.
+
+  Record union_find :=
+    MkUF {
+        rank : array t;
+        parent : array t;
+        (* we include an upper bound on the rank for purposes of termination *)
+        max_rank : t
+      }.
+
+  Definition length uf := length uf.(parent).
+  
+  Definition empty : union_find :=
+    MkUF (make zero) (make zero) zero.
+
+  (*TODO: write w/ state monad for clarity?*)
+  
+  Definition alloc '(MkUF ra pa mr) :=
+    let (i,pa) := alloc pa zero in
+    (*We don't need to initialize rank since default of 0 is correct*)
+    (MkUF ra pa.[i<-i] mr, i).
+
+  Definition find_aux
+    : t -> array t -> t -> array t * t :=
+    iter
+      (fun f i => (f,i))
+      (fun find_aux f i =>         
+      let fi := f.[i] in
+      if eqb fi i then
+        (f,i)
+      else
+        let (f, r) := find_aux f fi in
+        let f := f.[i<-r] in
+        (f,r)
+      ).
+
+  Definition find '(MkUF ra pa mr) x :=
+    let (f,cx) := find_aux mr pa x in
+    (MkUF ra f mr, cx).
+
+  
+  (*TODO: move to the right place*)
+  Definition max (x y : t) :=
+    if leb x y then y else x.
+
+  Definition union h x y :=
+    let (h, cx) := find h x in
+    let (h, cy) := find h y in
+    if eqb cx cy then h else
+      let (ra, pa, mr) := h in
+      let rx := ra.[cx] in
+      let ry := ra.[cy] in
+      if ltb ry rx then
+        MkUF ra pa.[cy <- cx] mr
+      else if ltb rx ry then
+             MkUF ra pa.[cx <- cy] mr
+           else
+             MkUF ra.[cx <- succ rx]
+                  pa.[cy <- cx]
+                       (max mr (succ rx)).
+End UnionFind.
+
+(*TODO*)
+Module UnionFindSpec
        (Import NL : Natlike)
        (Import AL : ArrayList NL)
        (Import ALS : ArrayListSpec NL AL).
-*)
+End UnionFindSpec.
 
-(* For functions with difficult termination conditions, use fuel.
-   Note: eventually we will use an upper bound on rank
-*)  
+(*Testing *)
+From Utils Require PersistentArrayList.
+
+Module UF63 := UnionFind.
+Import Int63 UF63.
+Import UF63.Ntns.
+
+
+Time Eval vm_compute in
+     let uf :=N.recursion empty
+                          (fun _  uf =>
+                             let (uf, i) := alloc uf in
+                             if ltb i 10 then uf else
+                               let uf := union uf i (sub i 10) in
+                               uf)
+                          100000 in
+     snd (find uf 64828%int63).
+
+  
