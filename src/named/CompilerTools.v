edfe0b8dfa8b25c591e241932464e66f3e9c7205
Dustin Jamner <dijamner@me.com>
2021-10-31 16:31:51 -0400

    Comment out theorem unil needed; might need revisions for newer elab

--- src/named/CompilerTools.v
+++ src/named/CompilerTools.v
@@ -6,19 +6,37 @@ Import ListNotations.
 Open Scope string.
 Open Scope list.
 From Utils Require Import Utils.
-From Named Require Import Core Compilers Elab ElabCompilers ElabCompilersWithPrefix.
+From Named Require Import Core Compilers Elab ElabCompilers.
 Import Core.Notations.
 (*TODO: repackage this in compilers*)
 Import CompilerDefs.Notations.
 
 Require Coq.derive.Derive.
 
+
+
+Section WithVar.
+  Context (V : Type)
+          {V_Eqb : Eqb V}
+          {V_default : WithDefault V}.
+
+  
+  Notation named_list := (@named_list V).
+  Notation named_map := (@named_map V).
+  Notation term := (@term V).
+  Notation ctx := (@ctx V).
+  Notation sort := (@sort V).
+  Notation subst := (@subst V).
+  Notation rule := (@rule V).
+  Notation lang := (@lang V).
+  Notation compiler := (@compiler V).
+
 Lemma elab_preserving_compiler_embed cmp_pre tgt cmp ecmp src tgt'
     : elab_preserving_compiler cmp_pre tgt cmp ecmp src ->
       incl tgt tgt' ->
       elab_preserving_compiler cmp_pre tgt' cmp ecmp src.
 Proof.
-  induction 1; basic_goal_prep; constructor; basic_core_crush.
+  induction 1; basic_goal_prep; constructor; basic_core_firstorder_crush.
   eapply eq_sort_lang_monotonicity; eauto.
   eapply eq_term_lang_monotonicity; eauto.
 Qed.
@@ -32,7 +50,9 @@ Lemma strengthen_named_list_lookup {A} (cmp : named_list A) n
                  named_list_lookup_err cmp' n = named_list_lookup_err cmp n.
 Proof.
   induction cmp; basic_goal_prep;  symmetry; basic_utils_crush.
-  my_case neq (n =? s); basic_goal_prep; basic_utils_crush.
+  my_case neq (eqb n v); basic_goal_prep; basic_utils_crush.
+  symmetry.
+  basic_core_crush.
 Qed.
 
 Lemma compile_strengthen_term_incl cmp e
@@ -41,7 +61,7 @@ Lemma compile_strengthen_term_incl cmp e
                  all_fresh cmp' ->
                  compile cmp' e = compile cmp e.
 Proof.
-  induction e; basic_goal_prep; basic_core_crush.
+  induction e; basic_goal_prep; basic_core_firstorder_crush.
   erewrite strengthen_named_list_lookup; eauto.
   case_match; basic_goal_prep;[| basic_core_crush].
   case_match; basic_goal_prep;[| basic_core_crush].
@@ -70,7 +90,7 @@ Lemma compile_strengthen_sort_incl cmp e
                  all_fresh cmp' ->
                  compile_sort cmp' e = compile_sort cmp e.
 Proof.
-  induction e; basic_goal_prep; basic_core_crush.
+  induction e; basic_goal_prep; basic_core_firstorder_crush.
   erewrite strengthen_named_list_lookup; eauto.
   case_match; basic_goal_prep;[| basic_core_crush].
   case_match; basic_goal_prep;[basic_core_crush|].
@@ -79,6 +99,13 @@ Proof.
   apply compile_strengthen_args_incl; eauto.
 Qed.
 
+
+(*We use a notation so that auto recognizes it after
+  reduction
+  TODO: move to end of Compilers.v
+*) 
+Notation all_constructors_ctx P c :=
+  (all (fun '(_,t) => all_constructors_sort P t) c).
 
 Lemma compile_strengthen_ctx_incl cmp e
   : all_constructors_ctx (fun n => In n (map fst cmp)) e ->
@@ -90,7 +117,7 @@ Proof.
     f_equal; [f_equal|]; firstorder eauto using compile_strengthen_sort_incl.
 Qed.
 
-Fixpoint constructor_names (l:lang) : list string :=
+Fixpoint constructor_names (l:lang) : list V :=
   match l with
   | (n,term_rule _ _ _)::l' => n::(constructor_names l')
   | (n,sort_rule _ _)::l' => n::(constructor_names l')
@@ -98,8 +125,8 @@ Fixpoint constructor_names (l:lang) : list string :=
   |[] => []
   end.
 
-Lemma preserving_compiler_constructor_names tgt cmp src
-  : preserving_compiler tgt cmp src ->
+Lemma preserving_compiler_constructor_names cmp_pre  tgt cmp src
+  : preserving_compiler_ext cmp_pre tgt cmp src ->
     map fst cmp = constructor_names src.
 Proof.
   induction 1; basic_goal_prep; basic_core_crush.
@@ -108,8 +135,13 @@ Qed.
 Lemma sort_rule_in_constructor_names n c args l
   : In (n, sort_rule c args) l -> In n (constructor_names l).
 Proof.
-  induction l; basic_goal_prep; basic_core_crush.
-  case_match; basic_core_crush.
+  induction l; basic_goal_prep; auto.
+  (*TODO: fix automation for this*)
+  repeat intuition break.
+  all:autorewrite with utils term lang_core in *.
+  intuition (subst; unshelve eauto 1 with utils term lang_core).
+  case_match;
+  intuition (subst; unshelve eauto  4 with utils term lang_core).
 Qed.
 Hint Resolve sort_rule_in_constructor_names : lang_core.
 
@@ -117,21 +149,26 @@ Hint Resolve sort_rule_in_constructor_names : lang_core.
 Lemma term_rule_in_constructor_names n c args t l
   : In (n, term_rule c args t) l -> In n (constructor_names l).
 Proof.
-  induction l; basic_goal_prep; basic_core_crush.
-  case_match; basic_core_crush.
+  induction l; basic_goal_prep; auto.
+  (*TODO: fix automation for this*)
+  repeat intuition break.
+  all:autorewrite with utils term lang_core in *.
+  intuition (subst; unshelve eauto 1 with utils term lang_core).
+  case_match;
+  intuition (subst; unshelve eauto  4 with utils term lang_core).
 Qed.
 Hint Resolve term_rule_in_constructor_names : lang_core.
 
 Local Lemma all_constructors_from_wf src
   : (forall c t,
         wf_sort src c t ->
-        all_constructors_sort (fun n0 : string => In n0 (constructor_names src)) t)
+        all_constructors_sort (fun n0 => In n0 (constructor_names src)) t)
     /\ (forall c e t,
            wf_term src c e t ->
-           all_constructors (fun n0 : string => In n0 (constructor_names src)) e)
+           all_constructors (fun n0 => In n0 (constructor_names src)) e)
     /\ (forall c s c',
            wf_args src c s c' ->
-           all (all_constructors (fun n0 : string => In n0 (constructor_names src))) s).
+           all (all_constructors (fun n0 => In n0 (constructor_names src))) s).
 Proof using.
   intros; apply wf_judge_ind; basic_goal_prep;
     with_rule_in_wf_crush.  
@@ -139,24 +176,24 @@ Qed.
 
 Definition all_constructors_sort_from_wf src
   := proj1 (all_constructors_from_wf src).
-#[export] Hint Resolve all_constructors_sort_from_wf : lang_core.
+Hint Resolve all_constructors_sort_from_wf : lang_core.
 
 Definition all_constructors_term_from_wf src 
   := proj1 (proj2 (all_constructors_from_wf src)).
-#[export] Hint Resolve all_constructors_term_from_wf : lang_core.
+Hint Resolve all_constructors_term_from_wf : lang_core.
 
 Definition all_constructors_args_from_wf src
   := proj2 (proj2 (all_constructors_from_wf src)).
-#[export] Hint Resolve all_constructors_args_from_wf : lang_core.
+Hint Resolve all_constructors_args_from_wf : lang_core.
 
 Lemma all_constructors_ctx_from_wf src c
   : wf_ctx src c ->
-    all_constructors_ctx (fun n0 : string => In n0 (constructor_names src)) c.
+    all_constructors_ctx (fun n0 => In n0 (constructor_names src)) c.
 Proof.
   induction 1; basic_goal_prep;
     with_rule_in_wf_crush.
 Qed.
-#[export] Hint Resolve all_constructors_ctx_from_wf : lang_core.
+Hint Resolve all_constructors_ctx_from_wf : lang_core.
 
 Require Import Compilers.
 
@@ -170,7 +207,7 @@ Lemma strengthen_named_list_lookup' {A} (ecmp cmp : named_list A) n
 Proof.
   induction ecmp; basic_goal_prep; basic_utils_crush.
   eapply strengthen_named_list_lookup; eauto.
-  my_case neq (n =? s); basic_goal_prep; basic_utils_crush.
+  my_case neq (eqb n v); basic_goal_prep; basic_utils_crush.
 Qed.
 
 
@@ -180,7 +217,7 @@ Lemma compile_strengthen_term_incl' ecmp cmp e
                  all_fresh cmp' ->
                  (compile (ecmp ++ cmp') e) = (compile (ecmp ++ cmp) e).
 Proof.
-  induction e; basic_goal_prep; basic_core_crush.
+  induction e; basic_goal_prep; basic_core_firstorder_crush.
   erewrite strengthen_named_list_lookup'; eauto.
   case_match; basic_goal_prep;[| basic_core_crush].
   case_match; basic_goal_prep;[| basic_core_crush].
@@ -209,7 +246,7 @@ Lemma compile_strengthen_sort_incl' ecmp cmp e
                  all_fresh cmp' ->
                  compile_sort (ecmp++cmp') e = compile_sort (ecmp++cmp) e.
 Proof.
-  induction e; basic_goal_prep; basic_core_crush.
+  induction e; basic_goal_prep; basic_core_firstorder_crush.
   erewrite strengthen_named_list_lookup'; eauto.
   case_match; basic_goal_prep;[| basic_core_crush].
   case_match; basic_goal_prep;[basic_core_crush|].
@@ -237,14 +274,15 @@ Qed.
 
 
 Lemma elab_preserving_compiler_monotonicity cmp' cmp_pre tgt cmp ecmp src src_pre cmp_pre'
-  : ElabCompilers.elab_preserving_compiler tgt cmp' cmp_pre src_pre ->
+  : elab_preserving_compiler [] tgt cmp' cmp_pre src_pre ->
     elab_preserving_compiler cmp_pre tgt cmp ecmp src ->
     wf_lang (src++src_pre) ->
     incl cmp_pre cmp_pre' ->
     all_fresh cmp_pre' ->
     elab_preserving_compiler cmp_pre' tgt cmp ecmp src.
 Proof.
-  induction 2; basic_goal_prep; autorewrite with utils lang_core in *; constructor; intuition eauto.
+  induction 2; basic_goal_prep; autorewrite with utils lang_core in *; try constructor; intuition eauto.
+  Abort (*TODO: update
   {
     erewrite compile_strengthen_ctx_incl'; eauto.
     eapply all_constructors_ctx_weaken; cycle 1.
@@ -343,7 +381,13 @@ Proof.
   }
 Qed.
 Hint Resolve elab_preserving_compiler_monotonicity : auto_elab.
+         *).
 
+End WithVar.
+
+
+(*TODO: move tactics? *)
+Require Import Matches.
    Ltac estep_under lang rule :=
       eredex_steps_with lang rule
       || (compute_eq_compilation; term_cong; (estep_under lang rule|| term_refl)).
