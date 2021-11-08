52c4470dcb19601ae5317bfc579e16b465277832
Dustin Jamner <dijamner@me.com>
2021-11-08 13:05:07 -0500

    Factor all_constructors out of compilers file

--- src/named/Compilers.v
+++ src/named/Compilers.v
@@ -6,7 +6,7 @@ Import ListNotations.
 Open Scope string.
 Open Scope list.
 From Utils Require Import Utils.
-From Named Require Import Core.
+From Named Require Import Core AllConstructors.
 Import Core.Notations.
 (*TODO: rename this to compiler proofs and have a separate export-only file?*)
 Require Export CompilerDefs.
@@ -63,23 +63,6 @@ Proof.
     basic_core_crush.
 Qed.
 Hint Resolve all_fresh_compiler : lang_core.
-
-Fixpoint all_constructors (P : V -> Prop) e :=
-  match e with
-  | var _ => True
-  | con n s => P n /\ all (all_constructors P) s
-  end.
-
-Definition all_constructors_sort P t :=
-  match t with
-  | scon n s => P n /\ all (all_constructors P) s
-  end.
-
-(*We use a notation so that auto recognizes it after
-  reduction
-*) 
-Notation all_constructors_ctx P c :=
-  (all (fun '(_,t) => all_constructors_sort P t) c).
 
 Lemma compile_strengthen_term cmp n cc e
   : fresh n cmp ->
@@ -857,14 +840,6 @@ Local Hint Resolve inductive_implies_semantic_ctx' : lang_core.
 
 Local Hint Constructors rule_compiles_with : lang_core.
 
-Definition all_constructors_rule P r :=
-  match r with
-  | sort_rule c _ => all (fun '(_, t) => all_constructors_sort P t) c
-  | term_rule c _ t =>
-    (all (fun '(_, t) => all_constructors_sort P t) c)
-    /\ (all_constructors_sort P t)
-  | _ => True
-  end.
 
 Local Lemma rule_compiles_extend_fresh lt cmp r n cc
   : rule_compiles_with lt cmp r ->
@@ -904,74 +879,6 @@ Proof.
 Qed.
 Hint Resolve all_constructors_rule_from_wf : lang_core.
 
-Lemma all_constructors_term_weaken (P Q : _ -> Prop) e
-  : (forall n, P n -> Q n) ->
-    all_constructors P e ->
-    all_constructors Q e.
-Proof.
-  intro.
-  induction e; basic_goal_prep; basic_core_firstorder_crush.
-
-  revert dependent l;
-    induction l; basic_goal_prep; basic_core_crush.
-Qed.
-Hint Resolve all_constructors_term_weaken : lang_core.
-
-
-Lemma all_constructors_args_weaken (P Q : _ -> Prop) l
-  : (forall n, P n -> Q n) ->
-    all (all_constructors P) l ->
-    all (all_constructors Q) l.
-Proof.
-  intro;
-    revert dependent l;
-    induction l; basic_goal_prep; basic_core_crush.
-Qed.
-Hint Resolve all_constructors_args_weaken : lang_core.
-
-
-Lemma all_constructors_sort_weaken (P Q : _ -> Prop) e
-  : (forall n, P n -> Q n) ->
-    all_constructors_sort P e ->
-    all_constructors_sort Q e.
-Proof.
-  intro.
-  destruct e; basic_goal_prep; basic_core_crush.
-Qed.
-Hint Resolve all_constructors_sort_weaken : lang_core.
-
-Lemma all_constructors_ctx_weaken (P Q : _ -> Prop) (c : ctx)
-  : (forall n, P n -> Q n) ->
-    all_constructors_ctx P c ->
-    all_constructors_ctx Q c.
-Proof.
-  intro;
-    induction c; basic_goal_prep; basic_core_crush.
-Qed.
-Hint Resolve all_constructors_ctx_weaken : lang_core.
-
-
-Lemma all_constructors_rule_weaken (P Q : _ -> Prop) r
-  : (forall n, P n -> Q n) ->
-    all_constructors_rule P r ->
-    all_constructors_rule Q r.
-Proof.
-  intro;
-    destruct r; basic_goal_prep; basic_core_crush.
-  all: eapply all_constructors_ctx_weaken; eauto.
-Qed.
-Hint Resolve all_constructors_rule_weaken : lang_core.
-
-
-Lemma all_constructors_lang_weaken (P Q : _ -> Prop) (l : lang)
-  : (forall n, P n -> Q n) ->
-    all (fun '(_, t) => all_constructors_rule P t) l ->
-    all (fun '(_, t) => all_constructors_rule Q t) l.
-Proof.
-  intro;
-    induction l; basic_goal_prep; basic_core_crush.
-Qed.
-Hint Resolve all_constructors_lang_weaken : lang_core.
 
 Lemma wf_lang_implies_all_constructors lt l
   : wf_lang l ->
@@ -1053,9 +960,3 @@ End WithVar.
 
 
 #[export] Hint Resolve all_constructors_rule_from_wf : lang_core.
-#[export] Hint Resolve all_constructors_term_weaken : lang_core.
-#[export] Hint Resolve all_constructors_args_weaken : lang_core.
-#[export] Hint Resolve all_constructors_sort_weaken : lang_core.
-#[export] Hint Resolve all_constructors_ctx_weaken : lang_core.
-#[export] Hint Resolve all_constructors_rule_weaken : lang_core.
-#[export] Hint Resolve all_constructors_lang_weaken : lang_core.
