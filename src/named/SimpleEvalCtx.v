5f36e1221953b1dc7bfe5f5574ab0d17bc091786
Gabriel Kammer <gkammer@mit.edu>
2022-04-28 12:03:07 -0400

    Add compiler generation

--- src/named/SimpleEvalCtx.v
+++ src/named/SimpleEvalCtx.v
@@ -6,7 +6,7 @@ Import ListNotations.
 Open Scope string.
 Open Scope list.
 From Utils Require Import Utils.
-From Named Require Import Core Elab SimpleVSubst SimpleVSTLC Matches SimpleVSum SimpleVProd.
+From Named Require Import CompilerDefs Core Elab SimpleVSubst SimpleVSTLC Matches SimpleVSum SimpleVProd.
 Import Core.Notations.
 
 Require Coq.derive.Derive.
@@ -261,9 +261,11 @@ Fixpoint eval_ctx_lang (hints : list (string * string * list string)) (l : lang)
 Definition Eapp_l_hint := ["e'"; "E"].
 Definition Eapp_r_hint := ["E"; "v"].
 
+Definition Estlc_hints := [("app", "Eapp_l", Eapp_l_hint);
+    ("app", "Eapp_r", Eapp_r_hint)].
+
 Definition auto_Estlc_def :=
-  eval_ctx_lang [("app", "Eapp_l", Eapp_l_hint);
-    ("app", "Eapp_r", Eapp_r_hint)] stlc_def.
+  eval_ctx_lang Estlc_hints stlc_def.
 
 Derive Estlc'
        SuchThat (elab_lang_ext (eval_ctx ++ stlc ++ exp_subst++ value_subst) auto_Estlc_def Estlc')
@@ -292,3 +294,167 @@ Derive Eprod
        As Eprod_wf.
 Proof. auto_elab. Qed.
 #[export] Hint Resolve Eprod_wf : elab_pfs.
+
+Locate term.
+
+
+
+
+Fixpoint wkn_n n :=
+  match n with
+  | 0 => {{e #"id"}}
+  | 1 => {{e #"wkn"}}
+  | S n' =>
+    {{e #"cmp" #"wkn" {wkn_n n'} }}
+  end.
+
+Definition bind_k n e A k :=
+  {{e #"blk_subst" (#"snoc" {wkn_n n} (#"cont" {A} {k})) {e} }}.
+Arguments bind_k n e A k/.
+
+Definition ovar n :=
+    {{e #"val_subst" {wkn_n n} #"hd" }}.  
+
+Definition original_app_rule_term : term := 
+  (bind_k 1 (var "e") {{e #"neg" (#"prod" "A" (#"neg" "B"))}}
+    (bind_k 2 (var "e'") (var "A")
+    {{e #"jmp" {ovar 1} (#"pair" {ovar 0} {ovar 2}) }})).
+
+Definition original_app_rule : compiler_case string :=
+  term_case ["e'"; "e"; "B"; "A"; "G"] original_app_rule_term.
+
+Definition app_compiler : compiler string :=
+  [("app", original_app_rule)].
+
+Compute original_app_rule.
+
+
+Check bind_k.
+
+Inductive bind_term : Type :=
+| bvar : string -> bind_term
+| bcon : string -> list bind_term -> bind_term
+| bbind_k : nat -> bind_term -> bind_term -> bind_term -> bind_term
+.
+
+Fixpoint match_wkn_n (t : term) : option nat :=
+  match t with
+  | con "id" [] => Some 0
+  | con "wkn" [] => Some 1
+  | con "cmp" [t'; con "wkn" []] =>
+      match match_wkn_n t' with
+      | Some n => Some (S n)
+      | None => None
+      end
+  | _ => None
+  end.
+
+
+Fixpoint term_to_bind_term (t : term) : bind_term :=
+  match t with
+  | var v => bvar v
+  | con "blk_subst" l => 
+      let default := bcon "blk_subst" (map (fun x => term_to_bind_term x) l) in
+      match l with
+      | [e; s] =>
+        match s with
+        | con "snoc" [con "cont" [k; A]; w] =>
+            match match_wkn_n w with
+            | None => default
+            | Some n =>
+                let e := term_to_bind_term e in
+                let A := term_to_bind_term A in
+                let k := term_to_bind_term k in
+                bbind_k n e A k
+            end
+        | _ => default
+        end
+      | _ => default
+      end
+  | con n l => bcon n (map (fun x => term_to_bind_term x) l)
+  end.
+
+
+
+Compute term_to_bind_term original_app_rule_term.
+
+Fixpoint bind_term_to_term (t : bind_term) : term :=
+  match t with
+  | bvar v => var v
+  | bcon n l => con n (map (fun x => bind_term_to_term x) l)
+  | bbind_k n e A k =>
+      let e := bind_term_to_term e in
+      let A := bind_term_to_term A in
+      let k := bind_term_to_term k in
+      {{e #"blk_subst" (#"snoc" {wkn_n n} (#"cont" {A} {k})) {e} }}
+  end.
+
+Compute bind_term_to_term (term_to_bind_term original_app_rule_term).
+Compute original_app_rule_term.
+
+Fixpoint substitute_ctx_bind_term (renamings : evctx_renamings) (e : bind_term) :=
+  match e with
+  | bvar n => bvar (rename_variable renamings n)
+  | bcon l args => bcon l (map (substitute_ctx_bind_term renamings) args)
+  | bbind_k n e' A k =>
+      let A' := substitute_ctx_bind_term renamings A in
+      let k' := substitute_ctx_bind_term renamings k in
+      let e'' := match e' with
+                | bvar v => 
+                    match find (fun x => eqb v x.(from)) renamings.(vals) with
+                    | Some replace_val =>
+                        let v := replace_val.(to) in
+                        term_to_bind_term {{e #"jmp" #"hd" (#"val_subst" #"wkn" v)}}
+                    | None => substitute_ctx_bind_term renamings e'
+                    end
+                 | _ => substitute_ctx_bind_term renamings e'
+                 end
+      in
+      bbind_k n e'' A' k'
+  end.
+
+
+Definition substitute_ctx_term (renamings : evctx_renamings) (e : term) :=
+  bind_term_to_term (substitute_ctx_bind_term renamings (term_to_bind_term e)).
+
+
+Definition simple_generate_eval_ctx_compiler_case'  (renamings : evctx_renamings) (case : compiler_case string) : option (compiler_case string) :=
+  match case with
+  | sort_case _ _ => None
+  | term_case args e => let args' := substitute_eval_ctx_in_args renamings args in
+                       let e' := substitute_ctx_term renamings e in
+                       Some (term_case args' e')
+  end.
+
+Definition simple_generate_eval_ctx_compiler_case (hint : list string) (case : compiler_case string) (lang_rule : rule) : option (compiler_case string) :=
+  match lang_rule with
+  | term_rule n args _ => let renamings := get_renamings hint args in
+                         simple_generate_eval_ctx_compiler_case' renamings case
+  | _ => None
+  end.
+
+Definition to_named_list {A : Type} (n : string) (x : option A) : named_list A :=
+  match x with
+  | Some x' => [(n, x')]
+  | None => []
+  end.
+
+
+Fixpoint eval_ctx_compiler (hints : list (string * string * list string)) (c : compiler string) (l : lang) : compiler string :=
+  match hints with
+  | (old_name, term_name, hint) :: hints' =>
+      let new_rules :=
+        match find (fun x => match x with (n, r) => n =? old_name end) l with
+        | Some (rn, r) =>
+            match find (fun x => match x with (n, r) => n =? old_name end) c with
+            | Some (cn, cr) => to_named_list term_name (simple_generate_eval_ctx_compiler_case hint cr r)
+            | None => []
+            end
+        | None => []
+        end
+      in
+      new_rules ++ eval_ctx_compiler hints' c l
+  | [] => []
+  end.
+
+Compute eval_ctx_compiler Estlc_hints app_compiler stlc_def.
