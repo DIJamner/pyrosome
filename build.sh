2a9f75bb1b3b55cfeebeeaac2fbdf8b7c85ac936
Dustin Jamner <dijamner@me.com>
2022-08-30 04:00:45 -0400

    Add bedrock2 dependency

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #todo: clean if makefile exists
 coq_makefile -f _CoqProject -o Makefile
-cd ./coqutil
-make -j$1
-cd ..
 cd ./canonical-binary-tries
 make -j$1
+cd ..
+cd ./bedrock2
+git submodule update --init
+make bedrock2_noex -j$1
 cd ..
 make -j$1
