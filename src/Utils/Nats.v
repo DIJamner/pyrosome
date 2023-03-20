32d8f81f4dc0f70c7254b88e81646a0bd3556c13
Dustin Jamner <dijamner@me.com>
2023-03-20 12:51:15 -0400

    Move non-building files out of src directory. Fix some compilation warnings

--- src/Utils/Nats.v
+++ src/Utils/Nats.v
@@ -13,15 +13,15 @@ Lemma invert_eq_0_S x : 0 = S x <-> False.
 Proof.
   solve_invert_constr_eq_lemma.
 Qed.
-Hint Rewrite invert_eq_0_S : utils.
+#[export] Hint Rewrite invert_eq_0_S : utils.
 Lemma invert_eq_S_0 x : S x = 0 <-> False.
 Proof.
   solve_invert_constr_eq_lemma.
 Qed.
-Hint Rewrite invert_eq_S_0 : utils.
+#[export] Hint Rewrite invert_eq_S_0 : utils.
 
 Lemma invert_eq_S_S x y : S x = S y <-> x = y.
 Proof.
   solve_invert_constr_eq_lemma.
 Qed.
-Hint Rewrite invert_eq_S_S : utils.
+#[export] Hint Rewrite invert_eq_S_S : utils.
