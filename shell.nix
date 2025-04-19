ffe377411841f9f0b33fde629850e9455ca23680
dijamner <dijamner@me.com>
2025-04-19 12:14:19 -0400

    Update shell.nix to support a native-compute-enabled option and coq 8.20

--- shell.nix
+++ shell.nix
@@ -1,6 +1,19 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {},
+  native-enabled ? false
+}:
 
 with pkgs;
 mkShell {
-  buildInputs = [ coq_8_19 ];
+  buildInputs = if native-enabled
+                then [
+                  #Note: must use coq form opam because native_compute doesn't work well with the nix version
+                  #coq_8_20
+                  linuxPackages.perf
+                  opam
+                  pkg-config
+                  gcc
+                  bintools-unwrapped
+                  gmp
+                ]
+                else [ coq  ]; #8_20 intended, but that only exists on unstable                
 }
