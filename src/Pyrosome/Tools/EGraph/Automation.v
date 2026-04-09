5155efaa6cc9bc142521572c6bb300e2d5de0baf
dijamner <dijamner@me.com>
2026-04-09 11:45:04 -0400

    Clean up Automation.v

--- src/Pyrosome/Tools/EGraph/Automation.v
+++ src/Pyrosome/Tools/EGraph/Automation.v
@@ -49,28 +49,6 @@ Fixpoint term_depth (e : term string) :=
   | con _ l => 1 + (fold_left Pos.max (map term_depth l) 1)
   end.
 
-(*
-Instance my_analysis : analysis string string (option positive) :=
-  weighted_depth_analysis (fun a => if inb a.(atom_fn) ["val_subst"; "blk_subst"]
-                                    then Some 20
-                                    else Some 1 (*(length a.(atom_args))) *) ).
-
-Instance full_term_analysis : analysis string string (option (term string)) :=
-  {
-    analyze a r :=
-      (@! let args <- option_all r in
-        ret con a.(atom_fn) args);
-    analysis_meet r1 r2 :=
-    match r1, r2 with
-    | None, _ => r2
-    | _, None => r1
-    | Some e, Some e' =>
-        if Pos.ltb (term_depth e) (term_depth e')
-        then Some e else Some e'
-    end;
-  }.
-*)
-
 Instance depth_analysis : analysis string string (option positive) :=
   weighted_depth_analysis (fun a => Some 1).
 
@@ -86,74 +64,15 @@ Theorem egraph_sound
     wf_term l c e2 t ->
     Is_Success (fst (egraph_reducing_equal' l filter reversible inj_rules rebuild_fuel sat_fuel efuel red_fuel c e1 e2)) ->
     eq_term l c t e1 e2.
+Proof.
+  intros ? ? ? ?.
+  unfold egraph_reducing_equal'.
+  (*TODO: verify renaming.*)
 Admitted.
 
-Ltac egraph rule_transform n :=
-      lazymatch goal with
-        |- eq_term ?l ?c ?t ?e1 ?e2 =>
-          let l' := constr:(ctx_to_rules c ++ l) in
-          let rs := constr:(StringInstantiation.build_rule_set
-                              1000 (rule_transform l') l') in
-        let result := (eval vm_compute in
-                        (StringInstantiation.egraph_equal (*V:=string*) l' rs n c e1 e2 t)) in
-        lazymatch result with
-        | (?b, ?g (*, ?r *)) =>
-            (*
-            (*TODO: how to extract and then rename?*)
-            let lp_and_r' := constr:((rename_lang (ctx_to_rules c ++ l) r)) in
-            let e1p := constr:(rename_term (var_to_con e1) (snd lp_and_r')) in
-             *)
-            (*
-            let p :=
-              (eval native_compute in (
-                  let (x1,g0) := add_open_term Pos.succ sort_of (fst lp_and_r')
-                                   [] (fst e1p) g in
-                  (x1,extract_weighted subst_weight 1000 g0 x1)
-              ))
-            in
-            match p with
-            | (?x1,?e1') =>*)
-       (* let assign := eval vm_compute in (fst (run_query (@PositiveInstantiation.compat_intersect) rw1 7 g)) in*)
-            pose (lie_to_user (real:=g) tt) as graph;
-           
-              pose (lie_to_user (real:=rs) tt) as rules;
-                 idtac b (*"! Also," e1 "became" e1' "with id" x1
-        end*)
-         
-        | _ => fail "ill-formed!"
-        end
-    end.
-    (*TODO: debug rules?*)
 (* TODO: think about variable order for query performance
 
  *)
-
-
-(*
-Lemma egraph_simpl2_sound
-  : forall (rebuild_fuel cap fuel efuel : nat) (l : lang string)
-           (c : named_list string (sort string)) t e1 e2 e1' e2' debug
-           weights,
-       wf_lang l ->
-       wf_ctx (Model:= core_model l) c ->
-       wf_term l c e1 t ->
-       wf_term l c e2 t ->
-       Defs.PositiveInstantiation.egraph_simpl2'_progressive
-         (H1:= weighted_depth_analysis weights)
-         l rebuild_fuel cap fuel efuel c e1 e2 = (e1',e2', debug) ->
-       eq_term l c t e1' e2'->
-       eq_term l c t e1 e2.
-Admitted. *)
-
-(*TODO: remove the need for cap? *)
-(* TODO: make injective-aware version
-   TODO: currently broken
- *)
-(*
-Ltac egraph_simpl2 cap :=
-    compute_eq_compilation;
-    eapply (egraph_simpl2_sound 100 cap 100 100);
-    [prove_from_known_elabs| shelve | shelve | shelve | vm_compute; reflexivity | ].*)
 
 (*TODO: call Matches.t' or some other tactic to solve subgoals*)
 Ltac by_reduction' reversible inj_rules :=
