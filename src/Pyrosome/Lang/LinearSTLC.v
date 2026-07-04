d6bc3cec2b5d657082a71e05e60e6b4594fd99b0
dijamner <dijamner@me.com>
2026-07-04 11:03:43 -0400

    Automatically general injectivity rules

--- src/Pyrosome/Lang/LinearSTLC.v
+++ src/Pyrosome/Lang/LinearSTLC.v
@@ -8,7 +8,8 @@ Open Scope list.
 From Utils Require Import Utils.
 From Pyrosome Require Import Theory.Core Elab.Elab
   Tools.Matches Tools.Resolution
-  Tools.EGraph.TypeInference Tools.EGraph.ComputeWf
+  Tools.EGraph.TypeInference Tools.EGraph.InjRuleGen
+  Tools.EGraph.ComputeWf
   Lang.LinearSubst.
 Import Core.Notations.
 
@@ -118,17 +119,26 @@ Definition linear_stlc_def : lang :=
   ]}.
 *)
 
+(*
 Definition linear_stlc_injectivity :=
   [("only", ["A"]); ("linear_lambda", ["e"; "B"; "A"; "G"]); ("cmp", ["G3"; "G1"]);
    ("id", ["G"]); ("exp_subst", ["A"; "G"]); ("linear_app", ["e'"; "e"; "B"; "A"; "H"; "G"]);
    ("ret", ["v"; "A"; "G"]); ("val_subst", ["A"; "G"]); ("exp", ["A"; "G"]);
    ("exch", ["H"; "G"]); ("vsub", ["v"; "A"; "G"]); ("val", ["A"; "G"]);
-   ("sub", ["G'"; "G"]); ("lolli", ["t'"; "t"]); ("hd", ["A"])].
+   ("sub", ["G'"; "G"]); ("lolli", ["t'"; "t"]); ("hd", ["A"])].*)
+
+
+(* TODO: can't include the language's rules while debugging the language.
+   I actually want this to be incrementally expanded/computed with the language.
+ *)
+Definition linear_gen_schemas :=
+  Eval vm_compute in
+    gen_fundep_schemas 10 (linear_exp_subst ++ linear_value_subst).
 
 Definition linear_stlc :=
   Eval vm_compute in
-    (infer_lang_ext_simple (linear_exp_subst++linear_value_subst) linear_stlc_def
-       linear_stlc_injectivity).
+    (infer_lang_ext_simple_gen (linear_exp_subst++linear_value_subst) linear_stlc_def
+       (build_general_injection_rules linear_gen_schemas)).
 
 Lemma linear_stlc_wf : wf_lang_ext (linear_exp_subst++linear_value_subst) linear_stlc.
 Proof. compute_wf_lang. Qed.
